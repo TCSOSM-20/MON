f840f69c0f5e0151ef747751bf54d9693a9bb9a1
kasar <pkasar@vmware.com>
2019-04-19 03:57:58 -0700

    Implemented VMware infra collector plugin
    Change-Id: Id46ceb4955ef1c9e9b3389c1016e77a41b7fda1b
    Signed-off-by: kasar <pkasar@vmware.com>

--- osm_mon/collector/service.py
+++ osm_mon/collector/service.py
@@ -4,6 +4,7 @@ from typing import List
 
 from osm_mon.collector.infra_collectors.onos import OnosInfraCollector
 from osm_mon.collector.infra_collectors.openstack import OpenstackInfraCollector
+from osm_mon.collector.infra_collectors.vmware import VMwareInfraCollector
 from osm_mon.collector.metric import Metric
 from osm_mon.collector.utils import CollectorUtils
 from osm_mon.collector.vnf_collectors.juju import VCACollector
@@ -21,7 +22,8 @@ VIM_COLLECTORS = {
     "vio": VIOCollector
 }
 VIM_INFRA_COLLECTORS = {
-    "openstack": OpenstackInfraCollector
+    "openstack": OpenstackInfraCollector,
+    "vmware": VMwareInfraCollector
 }
 SDN_INFRA_COLLECTORS = {
     "onos": OnosInfraCollector
