b6233b3d90ca1ba617e476b915fba056cf7891f7
Benjamin Diaz <bdiaz@whitestack.com>
2019-10-09 12:36:50 -0300

    Migrates alarms to MongoDB
    Change-Id: Iec09f507f05f6694910bf5ea82b948372d837af3
    Signed-off-by: lavado <glavado@whitestack.com>

--- osm_mon/cmd/mon_evaluator.py
+++ osm_mon/cmd/mon_evaluator.py
@@ -26,13 +26,12 @@ import logging
 import sys
 
 from osm_mon.core.config import Config
-from osm_mon.core.database import DatabaseManager
 from osm_mon.evaluator.evaluator import Evaluator
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='osm-policy-agent')
-    parser.add_argument('--config-file', nargs='?', help='POL configuration file')
+    parser = argparse.ArgumentParser(prog='osm-mon-evaluator')
+    parser.add_argument('--config-file', nargs='?', help='MON configuration file')
     args = parser.parse_args()
     cfg = Config(args.config_file)
 
@@ -48,9 +47,6 @@ def main():
     log.info("Starting MON Evaluator...")
     log.debug("Config: %s", cfg.conf)
     log.info("Initializing database...")
-    db_manager = DatabaseManager(cfg)
-    db_manager.create_tables()
-    log.info("Database initialized correctly.")
     evaluator = Evaluator(cfg)
     evaluator.evaluate_forever()
 
--- osm_mon/server/service.py
+++ osm_mon/server/service.py
@@ -21,12 +21,10 @@
 # contact: bdiaz@whitestack.com or glavado@whitestack.com
 ##
 import logging
-import uuid
 
-from osm_mon.core import database
 from osm_mon.core.common_db import CommonDbClient
 from osm_mon.core.config import Config
-from osm_mon.core.database import AlarmRepository, Alarm, AlarmTagRepository
+from osm_mon.core.models import Alarm
 
 log = logging.getLogger(__name__)
 
@@ -38,41 +36,16 @@ class ServerService:
 
     def create_alarm(self,
                      name: str,
-                     threshold: str,
+                     threshold: float,
                      operation: str,
                      severity: str,
                      statistic: str,
                      metric_name: str,
                      tags: dict) -> Alarm:
-        database.db.connect()
-        try:
-            with database.db.atomic():
-                alarm = AlarmRepository.create(
-                    uuid=str(uuid.uuid4()),
-                    name=name,
-                    threshold=threshold,
-                    operation=operation.lower(),
-                    severity=severity.lower(),
-                    statistic=statistic.lower(),
-                    metric=metric_name
-                )
-                for k, v in tags.items():
-                    AlarmTagRepository.create(
-                        name=k,
-                        value=v,
-                        alarm=alarm
-                    )
-                return alarm
-
-        finally:
-            database.db.close()
+        alarm = Alarm(name, severity, threshold, operation, statistic, metric_name, tags)
+        self.common_db.create_alarm(alarm)
+        return alarm
 
     def delete_alarm(self,
                      alarm_uuid: str) -> None:
-        database.db.connect()
-        try:
-            with database.db.atomic():
-                alarm = AlarmRepository.get(Alarm.uuid == alarm_uuid)
-                alarm.delete_instance()
-        finally:
-            database.db.close()
+        self.common_db.delete_alarm(alarm_uuid)
